6d70775464809475b6b3b7697ac00486d6d628bf
mrgh0st9158 <athosclpimenta@gmail.com>
2026-04-16 22:15:28 -0300

    Primeiras impressões com JavaScript

---
+++ 01 Javascript/testes.js
@@ -0,0 +1,18 @@
+// alert("Olá mundo!")
+// alert("SEGUNDA MENSAGEM")
+
+// let nome
+// let idade = 16
+
+// alert("Idade: " + idade)
+
+// alert("Nome: " + nome + "\nIdade: " + idade)
+
+// nome = prompt("Digite seu nome: ")
+// alert("Nome: " + nome)
+
+let nota1
+nota1 = prompt("Digite a primeira nota: ")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
