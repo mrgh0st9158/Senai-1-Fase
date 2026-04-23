d4fe403bf502c45c417ea34d63461d939f60a9dc
mrgh0st9158 <athosclpimenta@gmail.com>
2026-04-23 20:30:38 -0300

    Alguns exercícios básicos de JS

--- 01 Javascript/testes.js
+++ 01 Javascript/testes.js
@@ -11,8 +11,27 @@
 // nome = prompt("Digite seu nome: ")
 // alert("Nome: " + nome)
 
-let nota1
-nota1 = prompt("Digite a primeira nota: ")
-nota1 = Number(nota1)
-nota1 = nota1 + 1
-alert(nota1)
+// let nota1
+// nota1 = prompt("Digite a primeira nota: ")
+// nota1 = Number(nota1)
+// nota1 = nota1 + 1
+// alert(nota1)
+
+//=====================
+// informações
+let vitorias, empates
+let pontos
+
+//leituras
+vitorias = prompt("Total de vitórias:")
+vitorias = Number(vitorias)
+
+empates = Number(prompt("Total de empates:"))
+
+//processamento
+pontos = vitorias * 3 + empates
+
+//saídas
+alert("O total de pontos foi equivalente a: " + pontos)
+
+//=====================
