ff6fdf5b422910ad1850490098a462894db2174c
mrgh0st9158 <athosclpimenta@gmail.com>
2026-05-19 22:15:25 -0300

    trabalhos com variaveis locais e globais

--- 04 Variáveis/script.js
+++ 04 Variáveis/script.js
@@ -1,3 +1,106 @@
+// consertar código de dinossauros urgentemente
+
+let numero_aleatorio_aposta
+let valor_apostado
+
+function registrar_aposta(){
+    valor_apostado = Number(document.getElementById("input_aposta").value)
+    numero_aleatorio_aposta = Math.ceil(Math.random() * 10)
+
+    console.log(numero_aleatorio_aposta)
+
+}
+
+function botao_espinossauro(){
+    if(numero_aleatorio_aposta == 1){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+
+}
+
+function botao_trex(){
+     if(numero_aleatorio_aposta == 2){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+}
+
+function botao_braquiossauro(){
+     if(numero_aleatorio_aposta == 3){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+}
+
+function botao_velociraptor(){
+     if(numero_aleatorio_aposta == 4){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+}
+
+function botao_estegossauro(){
+     if(numero_aleatorio_aposta == 5){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+}
+
+function botao_triceratops(){
+     if(numero_aleatorio_aposta == 6){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+}
+
+function botao_anquilossauro(){
+     if(numero_aleatorio_aposta == 7){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+}
+
+function botao_parassaurolofo(){
+     if(numero_aleatorio_aposta == 8){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+}
+
+function botao_alossauro(){
+     if(numero_aleatorio_aposta == 9){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+}
+
+function botao_carnotauro(){
+     if(numero_aleatorio_aposta == 10){
+        valor_apostado = valor_apostado * 2
+
+        document.getElementById("aposta_recebida").innerHTML = ("Valor a receber: R$" + valor_apostado)
+    }
+}
+
+
+
+
+
+
+
+
+
+
 let numero_aleatorio
 let total = 0
 
@@ -6,20 +109,41 @@ let total = 0
 
      total = total + numero_aleatorio
 
-    document.getElementById("resultado_soma_dados").innerHTML = total
+    document.getElementById("resultado_soma_dados").innerHTML = "Resultado da soma dos dados: " + total
     document.getElementById("lista_dados").innerHTML += numero_aleatorio + "<br>"
 
  }
 
-//  function dado_d6(){
-//     let resultado = Math.ceil(Math.random()*6)
-    
-// }
+ function dado_d6(){
+    numero_aleatorio = Number(Math.ceil(Math.random() * 6))
 
-// function dado_d10(){
-//     let resultado = Math.ceil(Math.random()*6)
-    
-//  }
+    total = total + numero_aleatorio
+
+    document.getElementById("resultado_soma_dados").innerHTML = "Resultado da soma dos dados: " + total
+    document.getElementById("lista_dados").innerHTML += numero_aleatorio + "<br>"
+
+ }
+
+ function dado_d10(){
+    numero_aleatorio = Number(Math.ceil(Math.random() * 10))
+
+     total = total + numero_aleatorio
+
+    document.getElementById("resultado_soma_dados").innerHTML = "Resultado da soma dos dados: " + total
+    document.getElementById("lista_dados").innerHTML += numero_aleatorio + "<br>"
+
+ }
+
+
+function dado_d12(){
+    numero_aleatorio = Number(Math.ceil(Math.random() * 12))
+
+     total = total + numero_aleatorio
+
+    document.getElementById("resultado_soma_dados").innerHTML = "Resultado da soma dos dados: " + total
+    document.getElementById("lista_dados").innerHTML += numero_aleatorio + "<br>"
+
+}
 
 
 
