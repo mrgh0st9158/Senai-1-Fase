3e6793e2a9db4fe0d6ab61ba930ea350667424de
mrgh0st9158 <athosclpimenta@gmail.com>
2026-05-28 06:44:15 -0300

    site de dia dos namorados

---
+++ 05 Dia dos Namorados/script.js
@@ -0,0 +1,13 @@
+function dicas_namoro(){
+    document.getElementById("tela_inicial").style.display = "none"
+
+    document.getElementById("tela_dica_namoro").style.display = "block"
+
+}
+
+function caixa_bombom_dica(){
+    document.getElementById("p-dica_caixa_bombom").innerHTML = 
+    ("Que tal surpreender seu parceiro ou parceira com uma linda caixa de bombons. Isso aí, você não precisa nem avisar nada. Pode ser uma surpresa para ele(a). Tenho certeza de que ele(a) vai adorar.  (Você ganha pontos extras se tomar banho antes da surpresa!)")
+
+    document.getElementById("foto_gato_chocolate").style.display = "block"
+}
