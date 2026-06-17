faddd8c942c4695dd0691a313a05a264396ba68a
mrgh0st9158 <athosclpimenta@gmail.com>
2026-06-16 22:14:56 -0300

    atividades de js

--- 05 Repetição/script.js
+++ 05 Repetição/script.js
@@ -34,7 +34,16 @@ function anterior_tela(){
 }
 
 
+function reiniciar_processo(){
+      location.reload()
+      
+      todas_telas.forEach(function(todas_tela_funcao){
+            todas_tela_funcao.classList.add("esconder_elemento")
+      })
 
+      tela_atual.classList.remove("esconder_elemento")
+
+}
 
 
 function iniciar_historia(){
@@ -84,6 +93,46 @@ function iniciar_historia(){
                                 "\n4: Aceitar"+
                                 "\n5: Rejeitar"))
 
+                                if(escolha === 4){
+                                    alert("Roque aceita e marca Yasmina e os Manos em todas as postagens e o" +
+                                           "efeito é assustadoramente bom! O evento viraliza e todo mundo fica" +
+                                           "sabendo. Muita audiência, mas muita responsabilidade!" +
+
+                                          "Com todo essa promessa de sucesso, ofertas de parceria e patrocínios" +
+                                          "apareceram. Bandas também." +
+
+                                          "Uma desconhecida banda chamada Horrível Trio se ofereceu para tocar." +
+                                          "As músicas deles são muito relaxantes e Roque ficou receoso que colocasse" +
+                                          "o público para dormir.")
+
+
+                                    prompt("Roque deve aceitar Horrível Trio?" +
+
+                                           "- Sim: 08"+
+                                           "- Não: 09")
+
+                                }if(escolha === 5){
+                                      alert("Roque mente para as Tias Fofinhas que tem as toalhas. Elas perguntam" +
+                                            "de que cor são, ele diz que são brancas (afinal todas as toalhas de crochê" +
+                                            "são brancas, não é?). Elas complementam que suas toalhas tem que ser pretas." +
+                                            "Eles gagueja, diz que se enganou e que elas são pretas." +
+                                                  
+                                            "Elas desconfiam dele, fazem pressão para ver as toalhas e ele acaba" +
+                                            "reconhecendo que mentiu. Elas ficam muito decepcionadas e desistem de" +
+                                            "participar. Ao saber disso, a banda Jota Qwert também pula fora do barco." +
+                                           
+                                            "Ao ver Roque abatido com o iminente fracasso, seu pai, Lucio Fernando, oferece" +
+                                            "a Roque o estúdio e o salão da associação dos funcionários da sua fábrica, a Meia" +
+                                            "Meia Meia, para realizar o evento. Assinando um contrato ele também teria disponível o" +
+                                            "dinheiro necessário para trazer as bandas que quisesse." +
+                                           
+                                            "Roque nunca deixou o dinheiro e a influência de seu pai interferir em sua" +
+                                            "carreira artística e se sentiu incomodado com a proposta. Ao mesmo tempo, ficou" +
+                                            "balançado porque esse contrato seria a chance de realizar o maior sonho da sua" +
+                                            "vida!")
+                                           
+                                           
+                                          }
 
     }if(escolha == 3){
         alert("Roque contou para  as Tias Fofinhas que o evento ainda está engatinhando" +
@@ -105,5 +154,121 @@ function iniciar_historia(){
                                 "- Rejeitar: 05"))
 
     }
+
+}
+
+
+
+function iniciar_repetições(){
+      let total = 0
+      let i = 0
+    
+
+      while(i < 3){
+            i++
+             let preco = Number(prompt("Digite o preço do " + i + "° produto"))
+            total = total + preco
+
+
+            document.getElementById("p-resposta").innerHTML += i + "° produto: R$" + preco.toFixed(2) + "<br>"
+                                                  
+      }
+
+      document.getElementById("p-resposta").innerHTML += "<br>Valor total: R$" + total.toFixed(2)
+
+      setTimeout(function(){
+            reiniciar_processo()
+      },4000)
+      
+}
+
+function calcular_quadrado(){
+
+      let i = 0
+      let numero = 0
+      while(i < 10){
+            i++
+            numero = Number(prompt("Digite o " + i + "° Número"))
+            let quadrado = numero * numero
+
+
+
+            console.log(i + ": " + quadrado + "<br>")
+      }
+
+      setTimeout(function(){
+            reiniciar_processo()
+      },4000)
+
+}
+
+function imprimir_10_a_1(){
+      let i = 0
+      let numero = 10
+
+      while(i < 10){
+            i++
+            
+            document.getElementById("p-resposta").innerHTML += (numero + "<br>")
+
+            numero--
+      }
+
+
+      setTimeout(function(){
+            reiniciar_processo()
+      },4000)
+}
+
+function soma_1_a_100(){
+      let i = 0
+      let soma = 0
+
+      while(i < 100){
+            i++
+            soma = i + soma
+            
+            document.getElementById("p-resposta").innerHTML = "O resultado é equivalente a: " + soma
+      }
+
+      setTimeout(function(){
+            reiniciar_processo()
+      },4000)
+
+}
+
+function tabuada(){
+      let i = 0
+      let n_tabuada
+      let resposta
+
+      n_tabuada = Number(prompt("Qual tabuada você deseja?"))
+
+      while(i < 10){
+            i++
+            resposta = n_tabuada * i
+
+
+            document.getElementById("p-resposta").innerHTML += (resposta + "<br>")
+      }
+
+      setTimeout(function(){
+            reiniciar_processo()
+      },4000)
+
+}
+
+function fizzbuzz(){
+      let i = 0
+      let numero = 100
+      let resposta
+
+      while(i < 100){
+            i++
+            
+
+            document
+
+      }
 
 }
