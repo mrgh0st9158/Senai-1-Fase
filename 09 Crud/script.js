159f0b8dec191522355d70045d63787f0f3f4ce6
mrgh0st9158 <athosclpimenta@gmail.com>
2026-07-14 22:14:43 -0300

    crud

--- 09 Crud/script.js
+++ 09 Crud/script.js
@@ -7,17 +7,45 @@
 //D >> Delete >> Apagar
 
 
-const dinos = []
+let dinos = []
 
-function testar() {
-    let add_dino = {
-        nome: prompt("Nome: "),
-        altura: 3,
-        cor: "marrom",
-        custo: 14
+function limpar_input(){
+    document.getElementById("input_nome").value = ""
+    document.getElementById("input_altura").value = ""
+    document.getElementById("input_cor").value = ""
+    document.getElementById("input_custo").value = ""
+
+    document.getElementById("input_nome").focus()
+}
+
+function cadastrar_dino(){
+    const novo_dino = {
+        id: Date.now(),
+        nome: document.getElementById("input_nome").value,
+        altura: Number(document.getElementById("input_altura").value),
+        cor: document.getElementById("input_cor").value,
+        custo: Number(document.getElementById("input_custo").value)
     }
 
-    dinos.push(add_dino)
+    dinos.push(novo_dino)
 
     console.log(dinos)
+
+    limpar_input()
+
+}
+
+function mostrar_todos(){
+    for(let i = 0; i < dinos.length; i++){
+        document.getElementById("div-resposta").innerHTML += "ID: " + dinos[i].id + "<br>"
+        document.getElementById("div-resposta").innerHTML += "Nome: " + dinos[i].nome + "<br>"
+        document.getElementById("div-resposta").innerHTML += "Altura: " + dinos[i].altura + "<br>"
+        document.getElementById("div-resposta").innerHTML += "Cor: " + dinos[i].cor + "<br>"
+        document.getElementById("div-resposta").innerHTML += "Custo: " + dinos[i].custo + "<br>"
+        document.getElementById("div-resposta").innerHTML += "========================== <br>"
+    }
+}
+
+function testar() {
+    
 }
